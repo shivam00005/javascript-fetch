24ab890db1354f38721e94e3e7b52f685cc72c65
Shivam Sharma <shivampramod95925@gmail.com>
2023-01-12 21:27:17 -0600

    retrived all data from jason file expcept next_evolution and previous_evolution

--- app.js
+++ app.js
@@ -9,8 +9,42 @@ for (let i = 0; i < data.pokemon.length; i++) {
     div.className = "card";
 
 
-    div.innerHTML = `<h2>${data.pokemon[i].id}</h2>
-    <img src="${data.pokemon[i].img}"/>`
+    div.innerHTML = `
+    <div class = image>
+      <img src ="${data.pokemon[i].img}">
+    </div>
+    <div class = content>
+    
+      <h3>${data.pokemon[i].num}</h3>
+      <h3>Name: ${data.pokemon[i].name}</h3>
+      <h3>${data.pokemon[i].type}</h3>
+      <h3>${data.pokemon[i].height}</h3>
+      <h3>${data.pokemon[i].weight}</h3>
+      <h3>${data.pokemon[i].candy}</h3>
+      <h3>${data.pokemon[i].candy_count}</h3>
+      <h3>${data.pokemon[i].egg}</h3>
+      <h3>${data.pokemon[i].spawn_chance}</h3>
+      <h3>${data.pokemon[i].spawn_time}</h3>
+      <h3>${data.pokemon[i].multipliers}</h3>
+      <h3>${data.pokemon[i].weaknesses}</h3>
+      <h3>${data.pokemon[i].next_evolution.length}</h3>
+`
+
+// need work here
+    for (let j = 0; j <= 1; j++) {
+        `
+      <h3>${data.pokemon[i].next_evolution[j]}</h3>
+      <h3>${data.pokemon[i].next_evolution[j]}</h3>
+`
+    }
+// need work here
+
+    `   
+          
+         
+</div>
+      
+ `
 
     output.appendChild(div);
 }
