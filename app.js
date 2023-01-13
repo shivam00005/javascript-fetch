b01611dc24308ab4247c1f017ac8be6191a16826
Shivam Sharma <shivampramod95925@gmail.com>
2023-01-12 23:51:52 -0600

    retrived both evolution data using javascript property ( hasOwnProperty)  and condition statement

--- app.js
+++ app.js
@@ -5,47 +5,64 @@ const output = document.getElementById("output");
 
 for (let i = 0; i < data.pokemon.length; i++) {
 
-    let div = document.createElement("div");
-    div.className = "card";
+  let div = document.createElement("div");
+  let next_evolution = document.createElement("div");
+  let prev_evolution = document.createElement("div");
+  div.className = "card";
 
 
-    div.innerHTML = `
-    <div class = image>
+  div.innerHTML = `
+    <div class = 'image'>
       <img src ="${data.pokemon[i].img}">
     </div>
-    <div class = content>
-    
-      <h3>${data.pokemon[i].num}</h3>
+    <div class = 'content'>  
+      <h3>Rank: ${data.pokemon[i].num}</h3>
       <h3>Name: ${data.pokemon[i].name}</h3>
-      <h3>${data.pokemon[i].type}</h3>
-      <h3>${data.pokemon[i].height}</h3>
-      <h3>${data.pokemon[i].weight}</h3>
-      <h3>${data.pokemon[i].candy}</h3>
-      <h3>${data.pokemon[i].candy_count}</h3>
-      <h3>${data.pokemon[i].egg}</h3>
-      <h3>${data.pokemon[i].spawn_chance}</h3>
-      <h3>${data.pokemon[i].spawn_time}</h3>
-      <h3>${data.pokemon[i].multipliers}</h3>
-      <h3>${data.pokemon[i].weaknesses}</h3>
-      <h3>${data.pokemon[i].next_evolution.length}</h3>
-`
-
-// need work here
-    for (let j = 0; j <= 1; j++) {
-        `
-      <h3>${data.pokemon[i].next_evolution[j]}</h3>
-      <h3>${data.pokemon[i].next_evolution[j]}</h3>
-`
-    }
-// need work here
-
-    `   
-          
-         
-</div>
+      <h3>Type: ${data.pokemon[i].type}</h3>
+      <h3>Height: ${data.pokemon[i].height}</h3>
+      <h3>Weight: ${data.pokemon[i].weight}</h3>
+      <h3>Candy: ${data.pokemon[i].candy}</h3>
+      <h3>Candy Count: ${data.pokemon[i].candy_count}</h3>
+      <h3>Egg: ${data.pokemon[i].egg}</h3>
+      <h3>Spawn Chance: ${data.pokemon[i].spawn_chance}</h3>
+      <h3>Avg Spawn: ${data.pokemon[i].spawn_time}</h3>
+      <h3>Multiplayer: ${data.pokemon[i].multipliers}</h3>
+      <h3>Weakness: ${data.pokemon[i].weaknesses}</h3>
+ </div>
       
- `
+`;
 
-    output.appendChild(div);
+
+
+  if (data.pokemon[i].hasOwnProperty('next_evolution')) {
+
+    for (let j = 0; j < data.pokemon[i].next_evolution.length; j++) {
+
+
+      next_evolution.innerHTML += `
+      <h3> Next Evolution</h3>
+      <h3>Rank: ${data.pokemon[i].next_evolution[j].num}</h3>
+      <h3>Name: ${data.pokemon[i].next_evolution[j].name}</h3>
+  `;
+      div.appendChild(next_evolution);
+
+    }
+  } else if (data.pokemon[i].hasOwnProperty('prev_evolution')) {
+    for (let j = 0; j < data.pokemon[i].prev_evolution.length; j++) {
+
+      prev_evolution.innerHTML += `
+      <h3> Prev Evolution</h3>
+      <h3>Rank: ${data.pokemon[i].prev_evolution[j].num}</h3>
+      <h3>Name: ${data.pokemon[i].prev_evolution[j].name}</h3>
+  `;
+
+      div.appendChild(prev_evolution);
+    }
+  }
+
+
+
+  output.appendChild(div);
+
 }
 
